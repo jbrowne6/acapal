541eaafcc6051b268a218542162e76978112db4e
Browne <james.browne@westpoint.edu>
2020-04-19 09:03:40 -0400

    Made timeouts dynamic.  Added between paper time.

--- acapal.py
+++ acapal.py
@@ -5,17 +5,27 @@ from selenium.common import exceptions
 from bs4 import BeautifulSoup
 import json
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
-SLEEPTIME = 2
-PAGETIME = .5
+SLEEPTIME = 3 # timeout for mainpage loads
+SHORTTIME = 1 # rest time between papers
+PAGETIME = .75 # timeout for within page loads
 
 def getCitations(paperID, myDict):
     browser = configureBrowser()
     query = "https://academic.microsoft.com/paper/" + paperID
     browser.get(query)
-    time.sleep(SLEEPTIME)
+    try:
+        element_present = EC.presence_of_element_located((By.XPATH, "//ma-card[@au-target-id='418']"))
+        WebDriverWait(browser, SLEEPTIME).until(element_present)
+    except exceptions.TimeoutException:
+        print ("Timed out waiting to find a Paper.")
+        browser.quit()
+        return None
     myHTML = BeautifulSoup(browser.page_source, features="html.parser")
-
+    
     try:
         while True:
             papersList = myHTML.find_all("ma-card", attrs={"au-target-id":"418"})
@@ -28,15 +38,22 @@ def getCitations(paperID, myDict):
 
             link = browser.find_element_by_xpath("//i[@au-target-id='382']")
             link.click()
-            print("found it, sleeping now.")
+            # Next two lines ensure tags are available before proceeding.  Will cause timeout error otherwise.
+            element_present = EC.presence_of_element_located((By.XPATH, "//ma-card[@au-target-id='418']"))
+            WebDriverWait(browser, PAGETIME).until(element_present)
             myHTML = BeautifulSoup(browser.page_source, features="html.parser")
-            time.sleep(PAGETIME)
 
     except exceptions.NoSuchElementException:
+        print ("This paper didn't have the right tags.")
         pass
     except exceptions.ElementClickInterceptedException:
+        print ("The chrome browser lost focus.")
+        pass
+    except exceptions.TimeoutException:
+        print ("Timed out looking for citations.")
         pass
     except:
+        print ("Something went wrong.  Not sure what.")
         pass
     browser.quit()
 
@@ -45,7 +62,13 @@ def findInitialPaper(paperName):
     query = "https://academic.microsoft.com/search?q=" + urllib.parse.quote(paperName)
     browser = configureBrowser()
     browser.get(query)
-    time.sleep(SLEEPTIME)
+    try:
+        element_present = EC.presence_of_element_located((By.XPATH, "//a[@au-target-id='398']"))
+        WebDriverWait(browser, SLEEPTIME).until(element_present)
+    except exceptions.TimeoutException:
+        print ("Timed out waiting to find initial Paper.")
+        exit()
+
     myHTML = BeautifulSoup(browser.page_source, features="html.parser")
 
     mainPaper = myHTML.find_all("ma-card")[0]
@@ -67,7 +90,12 @@ def lookUpPaper(paperID):
     query = "https://academic.microsoft.com/paper/" + paperID
     browser = configureBrowser()
     browser.get(query)
-    time.sleep(SLEEPTIME)
+    try:
+        element_present = EC.presence_of_element_located((By.XPATH, "//div[@au-target-id='72']"))
+        WebDriverWait(browser, SLEEPTIME).until(element_present)
+    except exceptions.TimeoutException:
+        print ("Timed out waiting to find initial Paper.")
+        exit()
     myHTML = BeautifulSoup(browser.page_source, features="html.parser")
     paperYear = myHTML.find_all("span", attrs={"class":"year"})[0].string
     paperLink = myHTML.find_all("a", attrs={"au-target-id":"19"})[0]["href"]
@@ -89,6 +117,8 @@ def getGenerations(paperName, numGens):
     for x in range(numGens):
         for paper in list(bigPaperList):
             if paper not in alreadySearched:
+                print("Taking a short break between papers.")
+                time.sleep(SHORTTIME)
                 alreadySearched.append(paper)
                 getCitations(paper,bigPaperList)
     return bigPaperList
@@ -109,9 +139,12 @@ def printTopN(myDict, topN, fileName):
     sortedPapers = sorted(myPaperDict.items(), key=lambda x: x[1], reverse=True)
     topPapers = []
 
+    print("Printing top {} to file.".format(topN))
     with open(fileName, "w") as myFile:
         for x in range(topN):
             paper = lookUpPaper(str(sortedPapers[x][0]))
+            print("Printing paper {} details.".format(x))
+            time.sleep(SHORTTIME)
             paperTimes = sortedPapers[x][1]
             myFile.write("Paper {}: Appeared {} times.\n".format(x+1, paperTimes))
             for stat in paper:
@@ -125,6 +158,6 @@ paperName = "FlashGraph: processing billion-node graphs on an array of commodity
 paperName = "Grandmaster level in StarCraft II using multi-agent reinforcement learning"
 
 myFileName = "paperList.json"
-myPaperDict = getGenerations(paperName, 3)
+myPaperDict = getGenerations(paperName, 2)
 savePaperList(myFileName, myPaperDict)
 printTopN(myPaperDict,15,"topN.txt")
